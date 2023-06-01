ea0ae74fd3f87ddf8c9c0c125b30ebbfbbf2a3dd
Kayra <kayra.kuurt@gmail.com>
2023-06-01 21:31:01 +0300

    s

---
+++ Assets/Scripts/Networking/NetworkPlayerSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UIElements;
+
+public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
+{
+    private GameObject spawnedObjectPrefab;
+    public override void OnJoinedRoom()
+    {
+        Vector3 spawnVec = new Vector3(-21.77f, -12.21f, 3.5f);
+        base.OnJoinedRoom();
+        spawnedObjectPrefab = PhotonNetwork.Instantiate("Network Player", spawnVec, transform.rotation);
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        PhotonNetwork.Destroy(spawnedObjectPrefab);
+    }
+}
