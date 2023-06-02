23aa6f4b957483204f438da1b45424c9d13f0a99
seyfkarademir <hackerahmet5461@gmail.com>
2023-06-02 14:44:33 +0300

    d

---
+++ Assets/buton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buton : MonoBehaviour
+{
+    public AudioSource au;
+
+    public void PlayMusic()
+    {
+        au.Play();
+    }
+}
